e52d4e9a139387ae769b196b92bd57da3d3e2732
Garcia <agustin.garcia@sovos.com>
2019-12-16 10:20:30 -0300

    Diet_Test.xlsx

--- source/diet.py
+++ source/diet.py
@@ -1,9 +1,20 @@
+import pandas as pan
+from pandas import ExcelWriter
+from pandas import ExcelFile
+import openpyxl
+import numpy as num
+
+
 class App():
     def __init__(self):
-        print("Welcome ! :)")
+        on = True
 
     def main(self):
-        pass
+        data = pan.DataFrame({'column1': [1,2,3,4],
+                              'column2': [1,2,3,4]})
+        writer = ExcelWriter('Diet_Test.xlsx')
+        data.to_excel(writer, 'Sheet1', index=False)
+        writer.save()
 
 if __name__=="__main__":
     myApp = App()
