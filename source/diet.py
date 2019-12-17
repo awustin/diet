a377dcee28228c8625958e4e5944c50e41e02430
Garcia <agustin.garcia@sovos.com>
2019-12-17 10:22:25 -0300

    Example with Series and DataFrame

--- source/diet.py
+++ source/diet.py
@@ -1,20 +1,22 @@
-import pandas as pan
+import numpy as np
+import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 import openpyxl
-import numpy as num
 
 
 class App():
     def __init__(self):
         on = True
 
-    def main(self):
-        data = pan.DataFrame({'column1': [1,2,3,4],
-                              'column2': [1,2,3,4]})
-        writer = ExcelWriter('Diet_Test.xlsx')
-        data.to_excel(writer, 'Sheet1', index=False)
-        writer.save()
+    def main(self):  
+        columns = ['Date', 'Name', 'H', 'P', 'G', 'E']
+        s1 = pd.Series([pd.datetime(2019,12,17), 'a', 12, 3.3, 6, 140], index=columns)
+        s2 = pd.Series([pd.datetime(2019,12,17), 'b', 1, 10, 5, 70], index=columns)
+        s3 = pd.Series([pd.datetime(2019,12,17), 'c', 74, 25, 22, 400], index=columns)
+        d = pd.DataFrame.from_dict([s1, s2, s3])
+        print(d)
+        d.to_csv('example.csv')
 
 if __name__=="__main__":
     myApp = App()
